ca90610c3c723b866f394b465851068a0a44637f
MikhailovIgor <iomihailov@gmail.com>
2021-06-25 17:24:38 +0300

    developed HomeScreen UI

---
+++ src/components/svgComponents/TriangleRedDown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Svg, {Path} from 'react-native-svg';
+
+const TriangleRedDown = () => (
+  <Svg
+    width="9"
+    height="6"
+    viewBox="0 0 9 6"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <Path d="M4.5 6L8.39711 0.75H0.602886L4.5 6Z" fill="#D9545B" />
+  </Svg>
+);
+
+export default TriangleRedDown;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,85 +1,8 @@
 import React from 'react';
 import {ScrollView, StatusBar, Text, View, StyleSheet} from 'react-native';
 
-import {COLORS} from '../constants/colors';
-import LogoGoldX from '../components/LogoGoldX';
-
-const HomeBlueHeader = () => {
-  return (
-    <View
-      style={{
-        width: '100%',
-        height: 300,
-        backgroundColor: COLORS.primary,
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        paddingTop: '12%',
-      }}>
-      <LogoGoldX />
-      <View>
-        <Text
-          style={{fontFamily: 'OpenSans-Regular', fontSize: 18, color: '#fff'}}>
-          Yor Account Value:
-        </Text>
-        <Text
-          style={{
-            fontFamily: 'OpenSans-SemiBold',
-            fontWeight: '800',
-            fontSize: 26,
-            color: '#fff',
-          }}>
-          $1,850.40 USD
-        </Text>
-      </View>
-      <View style={{width: '88%', height: 1, backgroundColor: '#6da6f2'}} />
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          width: '88%',
-        }}>
-        <View>
-          <Text
-            style={{
-              fontFamily: 'OpenSans-Regular',
-              fontSize: 18,
-              color: '#fff',
-            }}>
-            Cash Balance:
-          </Text>
-          <Text
-            style={{
-              fontFamily: 'OpenSans-SemiBold',
-              fontWeight: '800',
-              fontSize: 22,
-              color: '#fff',
-            }}>
-            $1,084.10 USD
-          </Text>
-        </View>
-        <View>
-          <Text
-            style={{
-              fontFamily: 'OpenSans-Regular',
-              fontSize: 18,
-              color: '#fff',
-            }}>
-            Metal Holdings:
-          </Text>
-          <Text
-            style={{
-              fontFamily: 'OpenSans-SemiBold',
-              fontWeight: '800',
-              fontSize: 22,
-              color: '#fff',
-            }}>
-            $755.30 USD
-          </Text>
-        </View>
-      </View>
-    </View>
-  );
-};
+import HomeBlueHeader from '../components/HomeScreenComponents/HomeBlueHeader';
+import MetalPersonalInfoCard from '../components/HomeScreenComponents/MetalPersonalInfoCard';
 
 const HomeScreen = () => {
   return (
@@ -89,9 +12,14 @@ const HomeScreen = () => {
         translucent
         backgroundColor={'transparent'}
       />
-      <ScrollView style={{width: '100%'}}>
+      <ScrollView
+        style={{width: '100%'}}
+        contentContainerStyle={{alignItems: 'center'}}>
         <HomeBlueHeader />
-        <Text>Home Screen</Text>
+        <MetalPersonalInfoCard />
+        <MetalPersonalInfoCard />
+        <MetalPersonalInfoCard />
+        <MetalPersonalInfoCard />
       </ScrollView>
     </View>
   );
