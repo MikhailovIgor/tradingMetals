bf8c83ff80223169b0472ab946e010a82ca27efb
Roman Sarviro <roman.sarviro@mail.ru>
2021-07-05 13:02:02 +0300

    SafeAreaProvider wrapper

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import AuthStack from './navigation/AuthStack';
 import BottomTabNavigator from './navigation/BottomTabNavigator';
 
 const App = () => {
-  const auth = true;
+  const auth = false;
 
   return (
-    <NavigationContainer>
-      {auth ? <BottomTabNavigator /> : <AuthStack />}
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        {auth ? <BottomTabNavigator /> : <AuthStack />}
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
